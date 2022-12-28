a967f9677c8188158e31c2804604888365affc2d
yunhe <yunhe@bytedance.com>
2022-12-28 15:54:36 +0800

    diy: font

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { add } from './js/add'
 import { sayhello } from './js/hello'
 import './css/index.css'
+import './css/iconfont.css'
 sayhello()
--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,28 @@ module.exports = {
   // entry: 默认src/index.js
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'diy.js'
+    filename: 'diy.js',
+    clean: true
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|jpe?g|gif)/,
+        type: 'asset',
+        generator: {
+          filename: 'images/[hash:10][ext][query]'
+        }
+      },
+      {
+        test: /\.(ttf)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'font/[hash:10][ext][query]'
+        }
       }
     ]
   },
